15627a24006a3781ac7abe30223b1d7bc5b5ba30
azhukov <alz@unisport.dk>
2015-09-03 16:39:05 +0300

    implemented the decorator

--- django_jsonp/django_jsonp/decorators.py
+++ django_jsonp/django_jsonp/decorators.py
@@ -1,1 +1,53 @@
+from functools import wraps
+import types
+import json
 
+from django.http.response import HttpResponse, HttpResponseBadRequest
+from django.views.decorators.http import require_GET
+from django.views.generic import View
+
+
+APPLICATION_JS = 'application/javascript'
+JSONP = 'jsonp'
+СALLBACK = 'callback'
+
+JSONP_TEMPLATE = u'{callback}({payload})'
+
+def get_callback(request):
+    return request.GET.get(СALLBACK, request.GET.get(JSONP, None))
+
+
+def jsonp_response(response, callback=None):
+    if not callback:
+        return HttpResponseBadRequest('No callback supplied')
+
+    if isinstance(response, dict):
+        return HttpResponse(content=JSONP_TEMPLATE.format(callback=callback, payload=json.dumps(response)))
+    elif isinstance(response, HttpResponse):
+        response.content = JSONP_TEMPLATE.format(callback=callback, payload=response.content)
+        response['Content-Type'] = APPLICATION_JS
+        return response
+    else:
+        raise NotImplementedError('Not supported response value')
+
+
+def jsonp(view):
+    if isinstance(view, types.FunctionType):
+        @require_GET
+        @wraps(view)
+        def jsonpfied_view(request, *args, **kwargs):
+            return jsonp_response(view(request, *args, **kwargs), callback=get_callback(request))
+        return jsonpfied_view
+
+    elif isinstance(view, View):
+        class JSONPfiedCBV(view):
+            http_method_names = ['get']  # only GET method is allowed for JSONP
+
+            def get(self, request, *args, **kwargs):
+                return jsonp_response(
+                    super(JSONPfiedCBV, self).get(request, *args, **kwargs),
+                    callback=get_callback(request))
+
+        return JSONPfiedCBV
+    else:
+        raise NotImplementedError('Only django CBVs and FBVs are supported')
