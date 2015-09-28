6feadbe6803b9d99a4d4b9a8a04c3d6e81852b7d
azhukov <alz@unisport.dk>
2015-09-28 13:20:51 +0300

    updates classifiers

--- setup.py
+++ setup.py
@@ -29,6 +29,8 @@ setup(name='django_jsonp',
                    'License :: OSI Approved :: MIT License',
                    'Operating System :: OS Independent',
                    'Programming Language :: Python',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.4',
                    'Topic :: Software Development :: Libraries :: Python Modules',
                    'Topic :: Utilities'],
       )
